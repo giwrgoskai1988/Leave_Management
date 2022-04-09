379d932d6e96cae992e5b5119b9b425fb9ee86a9
giwrgoskai1988 <giwrgoskai1988@gmail.com>
2022-04-09 20:38:40 +0300

    Added missing mapper profiles

--- LM.Application/Profiles/MappingProfile.cs
+++ LM.Application/Profiles/MappingProfile.cs
@@ -12,8 +12,17 @@ namespace LM.Application.Profiles
         {
             CreateMap<LeaveRequest, LeaveRequestDto>().ReverseMap();
             CreateMap<LeaveRequest, LeaveRequestListDto>().ReverseMap();
+            CreateMap<LeaveRequest, CreateLeaveRequestDto>().ReverseMap();
+            CreateMap<LeaveRequest, UpdateLeaveRequestDto>().ReverseMap();
+
+
             CreateMap<LeaveType, LeaveTypeDto>().ReverseMap();
+            CreateMap<LeaveType, CreateLeaveTypeDto>().ReverseMap();
+
+
             CreateMap<LeaveAllocation, LeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, CreateLeaveAllocationDto>().ReverseMap();
+            CreateMap<LeaveAllocation, UpdateLeaveAllocationDto>().ReverseMap();
         }
     }
 }
