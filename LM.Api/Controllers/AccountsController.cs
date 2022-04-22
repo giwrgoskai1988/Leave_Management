9c45880ba0e281897e55ce8a5399bd004a1e93e8
giwrgoskai1988 <giwrgoskai1988@gmail.com>
2022-04-22 18:32:27 +0300

    api settings and startup

---
+++ LM.Api/Controllers/AccountsController.cs
@@ -0,0 +1,30 @@
+﻿using LM.Application.Contracts.Identity;
+using LM.Application.Models.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LM.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+
+        public AccountsController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthResponse>> Login(AuthRequest request)
+        {
+            return Ok(await _authService.Login(request));
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<RegistrationResponse>> Register(RegistrationRequest request)
+        {
+            return Ok(await _authService.Register(request));
+        }
+    }
+}
--- LM.Api/Startup.cs
+++ LM.Api/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using LM.Application;
+using LM.Identity;
 using LM.Infrastructure;
 using LM.Persistence;
+using Microsoft.OpenApi.Models;
 
 namespace LM.Api
 {
@@ -15,15 +17,14 @@ namespace LM.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
+            AddSwaggerDoc(services);
+
             services.ConfigureApplicationServices();
             services.ConfigureInfrastructureServices(Configuration);
             services.ConfigurePersistenceServices(Configuration);
+            services.ConfigureIdentityServices(Configuration);
 
             services.AddControllers();
-
-            services.AddEndpointsApiExplorer();
-
-            services.AddSwaggerGen();
 
             services.AddCors(c =>
             {
@@ -41,6 +42,8 @@ namespace LM.Api
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseAuthentication();
+
             app.UseSwagger();
             app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "LM.Api v1"));
 
@@ -55,6 +58,50 @@ namespace LM.Api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+        }
+
+        void AddSwaggerDoc(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme. 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+
+                        },
+                        new List<string>()
+                      }
+                    });
+
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "HR Leave Management Api",
+
+                });
+
             });
         }
     }
